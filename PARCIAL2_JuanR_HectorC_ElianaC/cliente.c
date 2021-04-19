844a3438c11856e2c89d124f174c774e1488f4be
unknown <eacamayo@unicauca.edu.co>
2021-04-19 06:07:52 -0500

    comentarios

--- PARCIAL2_JuanR_HectorC_ElianaC/cliente.c
+++ PARCIAL2_JuanR_HectorC_ElianaC/cliente.c
@@ -15,19 +15,36 @@ void handle_connection(int c);
 
 int main (int argc, char * argv[]){
 	int s;
+	int port;
 	struct sockaddr_in addr;
-	if ( (s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+	
+	//Crear el socket
+	//dominio=IPv4, socket de flujo, protocolo por defecto
+	s= socket(PF_INET, SOCK_STREAM, 0);
+	if ( s < 0) {
 	perror("socket");
 		exit(EXIT_FAILURE);
 }
+	//2. Asociar el socket a una dirección (IPv4)
+	port=0;
+	if(argc =3){
+		port = atoi(argv[2]);
+	}
+	if(port<=0){
+		fprintf(stderr, "Puerto %s invalido\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+	//limpiar y configurar la direción a enlazar 
 	memset(&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(50000);//TODO argumento del main!
+	
+	addr.sin_family = AF_INET;	//Familia de direciones IPv4
+	addr.sin_port = htons(port);// Puerto 1111 con los bytes en orden de red
 	//TODO tomar la IP de los argumentos del main!
 	if (inet_aton("127.0.0.1",&addr.sin_addr) == 0) {
 		fprintf(stderr, "Invalid address!\n");
 		exit(EXIT_FAILURE);
 	}
+	//Conectar el servidor
 if (connect (s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) != 0) {
 	perror("connect");
 	exit(EXIT_FAILURE);
@@ -45,7 +62,7 @@ if (connect (s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) != 0) {
 	memset(buf, 0, MSGSIZE);
 	read(s, buf, MSGSIZE);
 	printf("Server sent: %s\n", buf);
-	finished = 0;
+	finished = 0;  
 	while (!finished) {
 //TODO leer texto por la entrada estandar
 //TODO enviar texto al servidor
--- PARCIAL2_JuanR_HectorC_ElianaC/servidor.c
+++ PARCIAL2_JuanR_HectorC_ElianaC/servidor.c
@@ -13,59 +13,81 @@
 void handle_connection(int c);
 
 int main (int argc, char * argv[]){
-
+	
 	int s ;
 	int c;
+	
+	//1. Creación de dirección
 	struct sockaddr_in addr;
+	
+	int port;
 	int valopc;
 	socklen_t len;
-  if ( (s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+	
+	//dominio= IPv4, socket de flujo, protocolo por defecto
+	if ( (s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 	valopc=1;
 	len=sizeof(valopc);
 	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (void*)&valopc,len);
-	memset(&addr, 0, sizeof(struct sockaddr_in));
-		memset(&addr, 0, sizeof(struct sockaddr_in));
-
-		 addr.sin_family = AF_INET;	
-		 addr.sin_port = htons(50000);//TODO argumento del main!
-	 //TODO tomar la ip de los argumentos del main! 
-	   addr.sin_addr.s_addr = INADDR_ANY;
-		 if (bind (s, (struct sockaddr *)& addr, sizeof(struct sockaddr_in)) != 0) {
-		 		perror("bind");
-				exit(EXIT_FAILURE);
-
-		 }
-		 if (listen(s, 10) != 0) {
-			 perror("bind");
-	   	exit(EXIT_FAILURE);
-		 }
-//Recibir la conexion del cliente
-		c = accept(s, NULL, 0);
-
-//TODO para permitir la conexion con multiples clientes,  elservidor debe crear // un hilo que atienda el socket c 
-//
-//atender la conexion 
-		handle_connection(c);
-		close(s);
-		exit(EXIT_SUCCESS);
-}
-void handle_connection(int c) {
-	char buf [MSGSIZE];
-	int finished;
-	//Enviar al cliente un mensaje de bienvenida
-	strcpy(buf, "Hello!");
-	write(c, buf, MSGSIZE);
-	finished = 0;
-	while (!finished) {
-		//TODO leer mensaje del cliente
-		///TODO si el mensaje del cliente comienza por "/exit",
-		//TODO leer texto por la entrada estandar
-		//TODO enviar texto al cliente
-		//TODO si el texto leido comienza por "/exit", terminar
+	
+	//2. Asociar el socket a una dirección (IPv4)
+	port=0;
+	if(argc > 1){ //Especificar un puerto
+		port= atoi(argv[1]);
 	}
-	close(c);
+	if(port <= 1023){
+		fprintf(stderr, "Puerto %s invalido\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	//limpiar y configurar la direción a enlazar 
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;	//Familia de direciones IPv4
+	addr.sin_port = htons(port);// Puerto 1111 con los bytes en orden de red
+	//TODO tomar la ip de los argumentos del main! 
+	addr.sin_addr.s_addr = INADDR_ANY; //Asociar a la interfaz de red "activa  "
+	
+	if (bind (s, (struct sockaddr *)& addr, sizeof(struct sockaddr_in)) != 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+		
+	}
+	//3. Informar al S.O que el socket esta configurado y listo para recibir conexiones "escuchando"
+	if (listen(s, 10) != 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	//4. Esperar a que un cliente se conecte
+	c = accept(s, NULL, 0);
+	if(c<0){
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	
+	//TODO para permitir la conexion con multiples clientes,  elservidor debe crear // un hilo que atienda el socket c 
+	
+	//atender la conexion 
+	handle_connection(c);
+	close(s);
+	exit(EXIT_SUCCESS);
 }
-
+	void handle_connection(int c) {
+		char buf [MSGSIZE];
+		int finished;
+		//Enviar al cliente un mensaje de bienvenida
+		strcpy(buf, "Hello!");
+		write(c, buf, MSGSIZE);
+		finished = 0;
+		while (!finished) {
+			//TODO leer mensaje del cliente
+			///TODO si el mensaje del cliente comienza por "/exit",
+			//TODO leer texto por la entrada estandar
+			//TODO enviar texto al cliente
+			//TODO si el texto leido comienza por "/exit", terminar
+		}
+		//cerrar socket
+		close(c);
+	}
+	
